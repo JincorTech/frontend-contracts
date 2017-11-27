c591d097d8a4aba7dd8823ca5843d6d593f2fd39
Alexander <aleksand-isaev@yandex.ru>
2017-11-27 13:28:07 -0800

    Fixed back button bug

--- src/containers/wizard/WizardWrapper/index.tsx
+++ src/containers/wizard/WizardWrapper/index.tsx
@@ -3,6 +3,7 @@ import { SFC } from 'react';
 import { RouteComponentProps } from 'react-router';
 import * as CSSModules from 'react-css-modules';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import Avatar from '../../../components/common/Avatar';
 import { StateMap as StateProps, prevStep, Step } from '../../../redux/modules/wizard/employmentAgreementWizard';
 
@@ -16,17 +17,30 @@ export type DispatchProps = {
 export type Props = RouteComponentProps<{}, {}> & ComponentProps & DispatchProps & StateProps;
 
 const WizardWrapper: SFC<Props> = (props) => {
+  const renderBackButtonBody = () => {
+    return (
+      <div>
+        <div styleName="icon">
+          {'<'}
+        </div>
+        <div styleName="caption">
+          {'New smart contract'}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div styleName="layout">
       <div styleName="header">
-        <div styleName="back" onClick={props.prevStep}>
-          <div styleName="icon">
-            {'<'}
+        {props.currentStep === 0 ?
+          <Link styleName="back" to={'/ctr/app/contracts'}>
+            {renderBackButtonBody()}
+          </Link> :
+          <div styleName="back" onClick={props.prevStep}>
+            {renderBackButtonBody()}
           </div>
-          <div styleName="caption">
-            {'New smart contract'}
-          </div>
-        </div>
+        }
         <div styleName="avatar">
           <Avatar src={'/src'} fullName={''} id={''}/>
         </div>
