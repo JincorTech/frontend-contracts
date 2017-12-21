7cacc9a6b8a76ef84576bbef9d602e800d8b0675
kotokur <aleksand-isaev@yandex.ru>
2017-12-21 11:43:20 +0400

    Feature/issue 36 (#50)
    * Closes #36. Avatar was added to the Topbar

--- src/sagas/app/profileCard.ts
+++ src/sagas/app/profileCard.ts
@@ -2,13 +2,13 @@ import { SagaIterator } from 'redux-saga';
 import { takeLatest, takeEvery, call, put, select, fork } from 'redux-saga/effects';
 import { initialize } from 'redux-form';
 import { Action } from '../../utils/actions';
-import { get, put as putFunc } from '../../utils/api';
+import { put as putFunc } from '../../utils/api';
+import BasePath from '../../config';
 
 import {
   changePassword, updateProfile,
   changeView, closeProfileCard,
-  LOGOUT, FETCH_PROFILE, setAvatar,
-  OPEN_PROFILE_CARD
+  LOGOUT, FETCH_PROFILE, setAvatar
 } from '../../redux/modules/app/profileCard';
 import { fetchUser } from '../../redux/modules/app/appWrapper';
 import { logout } from '../../redux/modules/app/app';
@@ -42,7 +42,7 @@ function* changePasswordIterator({ payload }: Action<PasswordFields>): SagaItera
   const body = { companyId: id, ...payload };
 
   try {
-    yield call(putFunc, '/employee/changePassword/', body);
+    yield call(putFunc, BasePath.CompaniesApiPath, '/employee/changePassword/', body);
     yield put(changePassword.success());
     yield put(changeView('buttons'));
   } catch (e) {
@@ -61,7 +61,7 @@ function* updateProfileIterator({ payload }: Action<ProfileFields>): SagaIterato
   const body = { profile: { ...payload } };
 
   try {
-    yield call(putFunc, '/employee/me/', body);
+    yield call(putFunc, BasePath.CompaniesApiPath, '/employee/me/', body);
     yield put(updateProfile.success());
     yield put(fetchUser());
     yield put(changeView('buttons'));
@@ -89,28 +89,11 @@ function* logoutSaga(): SagaIterator {
   );
 }
 
-function* fetchUserIterator(): SagaIterator {
-  try {
-    const { data } = yield call(get, '/employee/me/');
-    yield put(fetchUser.success(data));
-  } catch (e) {
-    yield put(fetchUser.failure(e));
-  }
-}
-
-function* fetchUserSaga(): SagaIterator {
-  yield takeLatest(
-    OPEN_PROFILE_CARD,
-    fetchUserIterator
-  );
-}
-
 export default function*(): SagaIterator {
   yield [
     fork(changePasswordSaga),
     fork(getProfileSaga),
     fork(updateProfileSaga),
-    fork(logoutSaga),
-    fork(fetchUserSaga)
+    fork(logoutSaga)
   ];
 }
