69696e09f67e5e59207cf5b077af403edf4fc7db
kotokur <aleksand-isaev@yandex.ru>
2017-12-25 15:33:49 +0400

    Closes #20, #51 (#59)
    * Closes #20. Fixed Verification popup styles
    * Closes #51. Formatted and validated verification code
    * Fixed bug with float numbers in verification code field
    * Added integer input type
    * Added precision parameter to the Input component

--- src/containers/employmentAgreement/CreateContractForm/index.tsx
+++ src/containers/employmentAgreement/CreateContractForm/index.tsx
@@ -294,7 +294,7 @@ class CreateContractForm extends React.Component<Props, any> {
             <DateInput disabled={!this.canEdit()} name={'contractDate'} caption={true} value={fields.contractDate} onClick={() => openDatePopup(FormDates.ContractDate)} description={'Contract date'} buttonText={'Pick date'} />
           </li>
           <li styleName={getFilledStyle(1)}>
-            <Input disabled={!this.canEdit()} name={'contractNumber'} caption={true} type="number" max={999999} value={fields.contractNumber} onChange={this.handleChange} styleName="text-input" placeholder={'Contract number'} />
+            <Input disabled={!this.canEdit()} name={'contractNumber'} caption={true} type="integer" max={999999} value={fields.contractNumber} onChange={this.handleChange} styleName="text-input" placeholder={'Contract number'} />
           </li>
           <li styleName={getFilledStyle(2)}>
             <Caption text={'Wallets'} />
@@ -332,12 +332,15 @@ class CreateContractForm extends React.Component<Props, any> {
           <li styleName={getFilledStyle(6)}>
             <Caption text={'Compensation'} />
             <div styleName="salary-text-input-container">
-              <Input disabled={!this.canEdit()} name={'salaryAmount'} caption={true} captionText={EthCurrencyName} type="number" max={9999999999} value={fields.salaryAmount} onChange={this.handleChange} styleName="text-input" placeholder={'Salary amount'} />
+              <Input disabled={!this.canEdit()} name={'salaryAmount'} caption={true}
+                    captionText={EthCurrencyName} type="number" max={9999999999} precision={6}
+                    value={fields.salaryAmount} onChange={this.handleChange} styleName="text-input"
+                    placeholder={'Salary amount'} />
               <div styleName="salary-input-caption">
                 <InputCaption text="Monthly"/>
               </div>
             </div>
-            <Input disabled={!this.canEdit()} name={'paymentsDay'} caption={true} type="number" min={1} max={31} value={fields.paymentsDay} onChange={this.handleChange} styleName="text-input" placeholder={'Day of payments'} />
+            <Input disabled={!this.canEdit()} name={'paymentsDay'} caption={true} type="integer" min={1} max={31} value={fields.paymentsDay} onChange={this.handleChange} styleName="text-input" placeholder={'Day of payments'} />
           </li>
           <li styleName={getFilledStyle(7)}>
             <Caption text={'Additional сlauses'} />
